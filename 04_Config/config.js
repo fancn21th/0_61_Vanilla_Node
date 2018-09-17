204b31d2c06664c316b59211e713736814549d41
alex.fan <alex.fan@okchem.com>
2018-09-17 18:20:00 +0800

    config file

---
+++ 04_Config/config.js
@@ -0,0 +1,27 @@
+// Container for all the environments
+const environments = {}
+
+// Staging environment
+environments.staging = {
+  'port': 3000,
+  'envName': 'staging'
+}
+
+// Production environment
+environments.production = {
+  'port': 5000,
+  'envName': 'production'
+}
+
+// Determine which environment was passed as a command-line argument
+const currentEnvironment =
+  typeof(process.env.NODE_ENV) === 'string' ?
+    process.env.NODE_ENV.toLowerCase() : ''
+
+// Check that the current environment is one of the environments above, if not, default to staging
+const environmentToExport=
+  typeof(environments[currentEnvironment]) === 'object' ?
+    environments[currentEnvironment] : environments.staging
+
+// Export the module
+module.exports = environmentToExport
