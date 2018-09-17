3329b19ddf91c046a08c8f23715aa64833427b0b
alex.fan <alex.fan@okchem.com>
2018-09-17 13:43:05 +0800

    simple server

--- 01_Simple_Server/index.js
+++ 01_Simple_Server/index.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const url  = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 const server = http.createServer((req, res) => {
 
@@ -9,7 +10,7 @@ const server = http.createServer((req, res) => {
   // Get the path
   const path = parsedUrl.pathname
 
-  const trimmedPath = path.trim().replace(/^\/+|\/+$/g, '')
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
   // Get the query string as an object
   const queryStringObject = parsedUrl.query
@@ -20,11 +21,27 @@ const server = http.createServer((req, res) => {
   // Get the headers as an object
   const headers = req.headers
 
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+
+  req.on('data', data => {
+    buffer += decoder.write(data)
+  })
+
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    res.end('Hello World\n')
+
+    console.log(`Request received with this payload: `, buffer)
+  })
+
   // Send the response
-  res.end('Hello World\n')
+  // res.end('Hello World\n')
 
   // Log the request path
-  console.log(`Requests received with these headers`, headers)
+  // console.log(`Requests received with these headers`, headers)
   // console.log(`Request received on path : ${trimmedPath} with method ${method} and with these query string parameteres `, queryStringObject)
 })
 
