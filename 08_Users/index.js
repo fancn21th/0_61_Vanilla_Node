4a20ed348937db0bbc243c7dccddee25f1f0c499
fancn <fanyj@yikeer.com>
2018-09-24 19:07:42 +0800

    08 create users

---
+++ 08_Users/index.js
@@ -0,0 +1,108 @@
+const http = require('http')
+const https = require('https')
+const url  = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
+const { httpPort, httpsPort, envName } = require('./config')
+const fs = require('fs')
+const handlers = require('./lib/handlers')
+const helpers = require('./lib/helpers')
+
+// Instantiate the HTTP server
+const httpServer = http.createServer((req, res) => {
+  unifiedServer(req, res)
+})
+
+// Start the HTTP server
+httpServer.listen(httpPort , () => {
+  console.log(`The server is listening on port ${httpPort} in ${envName} mode`)
+})
+
+// Instantiate the HTTPs server
+const httpsServerOptions = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+}
+
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+  unifiedServer(req, res)
+})
+
+// Start the HTTPs server
+httpsServer.listen(httpsPort , () => {
+  console.log(`The server is listening on port ${httpsPort} in ${envName} mode`)
+})
+
+
+// All the server logic for both http and https server
+// No port related
+const unifiedServer = (req, res) => {
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true)
+
+  // Get the path
+  const path = parsedUrl.pathname
+
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase()
+
+  // Get the headers as an object
+  const headers = req.headers
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8')
+  let buffer = ''
+
+  req.on('data', data => {
+    buffer += decoder.write(data)
+  })
+
+  req.on('end', () => {
+    buffer += decoder.end()
+
+    // Choose the handler this request should go to.
+    // If one is not found, use the notFound handler
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ?
+      router[trimmedPath] : handlers.notFound
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: helpers.parseJsonToObject(buffer)
+    }
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // Use the status code called back by the handler, or default to 200
+      statusCode = typeof(statusCode) === 'number' ? statusCode : 200
+
+      // Use the payload called back by the handler, or default to empty object
+      payload = typeof(payload) === 'object' ? payload : {}
+
+      // Conver the payload to a string
+      const payloadString = JSON.stringify(payload)
+
+      // Return the response
+      res.setHeader('Content-Type', 'application/json')
+      res.writeHead(statusCode)
+      res.end(payloadString)
+
+      // Log the response
+      console.log(`Returning this response: `, statusCode, payloadString)
+    })
+  })
+}
+
+// Define a request router
+
+const router = {
+  'ping' : handlers.ping,
+  'users' : handlers.users
+}
---
+++ 08_Users/lib/handlers.js
@@ -0,0 +1,122 @@
+/**
+*  Request handlers
+*/
+
+// Dependencies
+const _data = require('./data')
+const helpers = require('./helpers')
+
+// Define the handlers
+
+const handlers = {}
+
+const getValueByFiledName = (data, name) => {
+  const nestedNames = name.split('.')
+  let value = data
+  nestedNames.forEach(nestedName => {
+    value = value[nestedName]
+  })
+  return (value && value.trim()) || false
+}
+
+handlers.users = (data, callback) => {
+  const acceptableMethods = ['post', 'get', 'put', 'delete']
+  if (acceptableMethods.indexOf(data.method) > -1) {
+    handlers._users[data.method](data, callback)
+  } else {
+    callback(405)
+  }
+}
+
+// Container for the users submethods
+handlers._users = {}
+
+// Users - Post
+// Required data: firstName, lastName, phone, password, tosAgreements
+// Optional data: none
+handlers._users.post = (data, callback) => {
+
+  // Check that all required fields are filled out
+  const firstName = getValueByFiledName(data, 'payload.firstName')
+  const lastName = getValueByFiledName(data, 'payload.lastName')
+  const phone = getValueByFiledName(data, 'payload.phone').length == 10 ? getValueByFiledName(data, 'payload.phone') : false
+  const password = getValueByFiledName(data, 'payload.password')
+  const tosAgreement = typeof(data.payload.tosAgreement) == 'boolean' && data.payload.tosAgreement == true ? true : false
+
+  if (firstName && lastName && phone && password && tosAgreement) {
+    // Make sure that the user doesn't already exist
+    _data.read('users', phone, (err, data) => {
+      if(!err) {
+        // Hash the password
+        const hashedPassword = helpers.hash(password)
+
+        if (hashedPassword) {
+          // Create the user object
+          const userObject = {
+            firstName,
+            lastName,
+            phone,
+            hashedPassword,
+            tosAgreement: true
+          }
+
+          // Store the user
+          _data.create('users', phone, userObject, err => {
+            if(!err) {
+              callback(200)
+            } else {
+              console.log(err)
+              callback(500, {
+                'Error': 'Could not create the new user'
+              })
+            }
+          })
+        } else {
+          callback(500, {
+            'Error': 'Could not hash the user'
+          })
+        }
+
+      } else {
+        // User already exists
+        callback(400, {
+          'Error': 'A user with that phone number already exists'
+        })
+      }
+    })
+
+  } else {
+    callback(400, {
+      'Error': 'Missing required fields'
+    })
+  }
+
+}
+
+// Users - Get
+handlers._users.get = (data, callback) => {
+
+}
+
+// Users - Put
+handlers._users.put = (data, callback) => {
+
+}
+
+// Users - delete
+handlers._users.delete = (data, callback) => {
+
+}
+
+// Ping handler
+handlers.ping = (data, callback) => {
+  // Callback a http status code, and a payload object
+  callback(200)
+}
+
+handlers.notFound = (data, callback) => {
+  callback(404)
+}
+
+// Export the module
+module.exports = handlers
